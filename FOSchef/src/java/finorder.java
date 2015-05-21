789f4f068761e58e27a6292d7450ecd14f3e65ea
akshayx11 <akshaygupta@Akshays-MacBook-Air.local>
2015-05-21 13:02:09 +0530

    Chef Interface added

---
+++ FOSchef/src/java/finorder.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.sql.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class finorder extends HttpServlet {
+
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+             int ready= Integer.parseInt(request.getParameter("bytableno"));
+       try
+            {
+               
+                Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Connected to the database");
+             Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/dit","root","bitnami");
+            
+            
+            
+                PreparedStatement ps= con.prepareStatement("DELETE FROM fos WHERE tableno= ?");
+                
+                
+                ps.setInt(1,ready);
+                ps.executeUpdate();
+                response.sendRedirect("index.jsp");
+        }
+       catch(Exception e)
+       {
+           out.print(e);
+       }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
+
